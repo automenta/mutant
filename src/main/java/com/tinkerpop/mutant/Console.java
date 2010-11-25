57f81ae4b8b36351b0e2a1002d5ee7799d3571c3
okram <okrammarko@gmail.com>
2010-11-24 19:59:47 -0700

    added Groovy to script engine pool. Added some more ? functionality.

--- src/main/java/com/tinkerpop/mutant/Console.java
+++ src/main/java/com/tinkerpop/mutant/Console.java
@@ -1,6 +1,5 @@
 package com.tinkerpop.mutant;
 
-import com.tinkerpop.gremlin.compiler.context.VariableLibrary;
 import jline.ConsoleReader;
 import jline.History;
 
@@ -18,14 +17,18 @@ public class Console {
 
     List<ScriptEngine> scriptEngines = new ArrayList<ScriptEngine>();
     List<String> scriptNames = new ArrayList<String>();
-    Bindings bindings = new VariableLibrary();
+    Bindings bindings = new SimpleBindings();
     int currentEngine = -1;
+
     PrintStream output = System.out;
 
+    private static final String MUTANT_HISTORY = ".mutant_history";
+
     public Console() throws Exception {
+        System.setProperty("org.jruby.embed.localvariable.behavior", "persistent");
         ScriptEngineManager manager = new ScriptEngineManager();
         for (ScriptEngineFactory factory : manager.getEngineFactories()) {
-            this.output.println("Enabling " + factory.getEngineName() + "[" + factory.getLanguageName() + "]");
+            this.output.println("Initializing " + factory.getEngineName() + "[" + factory.getLanguageName() + "]");
             this.scriptEngines.add(factory.getScriptEngine());
             this.scriptNames.add(factory.getLanguageName());
         }
@@ -35,6 +38,7 @@ public class Console {
             this.currentEngine = 0;
         }
 
+        this.output.println("\nMuTanT v0.1 [?h = help]");
         this.primaryLoop();
     }
 
@@ -47,7 +51,7 @@ public class Console {
 
         try {
             History history = new History();
-            history.setHistoryFile(new File(".mutant-history"));
+            history.setHistoryFile(new File(MUTANT_HISTORY));
             reader.setHistory(history);
         } catch (IOException e) {
             System.err.println("Could not find history file.");
@@ -63,14 +67,23 @@ public class Console {
                 if (line.isEmpty())
                     continue;
                 else if (line.startsWith("?")) {
-                    if (line.equals("?quit"))
+                    if (line.equals("?q"))
                         return;
-                    else if (line.equals("?next"))
-                        this.currentEngine = (this.currentEngine + 1) % this.scriptEngines.size();
-                    else if (line.equals("?binds"))
-                        this.output.println(this.getBindingsString());
+                    else if (line.equals("?x"))
+                        this.moveCurrentEngine(1);
+                    else if (line.equals("?z"))
+                        this.moveCurrentEngine(-1);
+                    else if (line.equals("?b"))
+                        this.printBindings();
+                    else if (line.equals("?h"))
+                        this.printHelp();
+                    else if (line.equals("?e"))
+                        this.printEngines();
                 } else {
+                    //getScriptEngine().setBindings(this.bindings, ScriptContext.ENGINE_SCOPE);
+                    //getScriptEngine().setBindings(this.bindings, ScriptContext.GLOBAL_SCOPE);
                     this.output.println(getScriptEngine().eval(line, this.bindings));
+                    //this.bindings = getScriptEngine().getBindings(ScriptContext.GLOBAL_SCOPE);
                 }
             } catch (Exception e) {
                 this.output.println(e.getMessage());
@@ -78,12 +91,29 @@ public class Console {
         }
     }
 
-    public String getBindingsString() {
+    public void printHelp() {
+        this.output.println("?z: previous engine");
+        this.output.println("?x: next engine");
+        this.output.println("?b: show bindings");
+        this.output.println("?e: show engines");
+        this.output.println("?q: quit");
+
+    }
+
+    public void printEngines() {
         String string = new String();
-        for(String key : this.bindings.keySet()) {
+        for (String name : this.scriptNames) {
+            string = string + name + "\n";
+        }
+        this.output.println(string.trim());
+    }
+
+    public void printBindings() {
+        String string = new String();
+        for (String key : this.bindings.keySet()) {
             string = string + key + "=" + this.bindings.get(key) + "\n";
         }
-        return string.trim();
+        this.output.println(string.trim());
     }
 
     public String getPrompt() {
@@ -96,6 +126,15 @@ public class Console {
 
     public ScriptEngine getScriptEngine() {
         return this.scriptEngines.get(this.currentEngine);
+    }
+
+    public void moveCurrentEngine(int direction) {
+        this.currentEngine = this.currentEngine + direction;
+        if (this.currentEngine == -1)
+            this.currentEngine = this.scriptEngines.size() - 1;
+        else
+            this.currentEngine = this.currentEngine % this.scriptEngines.size();
+
     }
 
     public static void main(String[] args) throws Exception {
